000681915d52ab64d3f0cea9e053465acf986b96
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-19 14:18:31 +1300

    refactor: lazily initialize state, using matchMedia when available

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import { canUseMatchMedia } from "./canUseMatchMedia";
 import { useBrowserLayoutEffect } from "./useBrowserLayoutEffect";
 
 const useMatchMedia = (mediaQueryString, initialState = false) => {
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState(() =>
+    canUseMatchMedia
+      ? window.matchMedia(mediaQueryString).matches
+      : initialState,
+  );
 
   useBrowserLayoutEffect(() => {
     if (canUseMatchMedia) {
       const mediaQueryList = window.matchMedia(mediaQueryString);
 
-      const updateState = () => {
-        setState(mediaQueryList.matches);
-      };
+      const updateState = () => setState(mediaQueryList.matches);
       updateState();
 
       mediaQueryList.addListener(updateState);
-      return () => {
-        mediaQueryList.removeListener(updateState);
-      };
+      return () => mediaQueryList.removeListener(updateState);
     }
   }, [mediaQueryString]);
 
