f4a5937b3a449a971078f9d0f5490f88f526aa99
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-19 20:43:28 +1300

    chore: add eslint-plugin-jsdoc

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import js from "@eslint/js";
 import pluginVitest from "@vitest/eslint-plugin";
 import { defineConfig, globalIgnores } from "eslint/config";
 import configPrettier from "eslint-config-prettier/flat";
+import pluginJsdoc from "eslint-plugin-jsdoc";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 
 export default defineConfig([
   globalIgnores(["coverage", "dist"]),
   js.configs.recommended,
+  pluginJsdoc.configs["flat/recommended"],
   pluginReactHooks.configs.flat["recommended-latest"],
   configPrettier,
   {
