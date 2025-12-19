1810cdc45bb25f12b101e21cad318c6671baa0b1
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-19 17:03:48 +1300

    test: dynamically import useMatchMedia

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,19 @@
 // @vitest-environment jsdom
 import { act, renderHook } from "@testing-library/react";
 import matchMediaMock from "match-media-mock";
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
-import useMatchMedia from "..";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+
+beforeEach(() => {
+  vi.resetModules();
+});
 
 describe("window.matchMedia is supported", () => {
-  beforeEach(() => {
+  let useMatchMedia;
+
+  beforeEach(async () => {
     window.matchMedia = matchMediaMock.create();
     window.matchMedia.setConfig({ type: "screen", width: 1200 });
+    useMatchMedia = (await import("..")).default;
   });
 
   afterEach(() => {
@@ -55,6 +61,12 @@ describe("window.matchMedia is supported", () => {
 });
 
 describe("window.matchMedia is not supported", () => {
+  let useMatchMedia;
+
+  beforeEach(async () => {
+    useMatchMedia = (await import("..")).default;
+  });
+
   test("returns false", () => {
     const { result } = renderHook(() => useMatchMedia("(max-width: 1280px)"));
     expect(result.current).toBe(false);
